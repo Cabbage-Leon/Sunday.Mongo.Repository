295a7bed8a36a5c9951d2f90c30938e19e4b52be
leon <leon.liu@homsom.com>
2021-09-10 15:34:27 +0800

    初始化

---
+++ src/Sunday.Mongo.Repository.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Sunday.Mongo.Repository.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
